27b694e83aec245436b40550c919bba591a50f1f
Allan Wazacz <cazwazacz@gmail.com>
2017-10-16 13:32:38 +0100

    all tests

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,3 +1,11 @@
 def fizzbuzz n
-  'fizz'
+  if n % 5 == 0 and n % 3 == 0
+    'fizzbuzz'
+  elsif n % 5 == 0
+    'buzz'
+  elsif n % 3 == 0
+    'fizz'
+  else
+    n
+  end
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,16 @@
 require 'fizzbuzz'
 
 describe 'fizzbuzz' do
-  it 'returns "fizz" when passed 3' do
+  it 'returns "fizz" when passed a multiple of 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+  it 'returns "buzz" when passed a multiple of 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+  it 'returns "fizzbuzz" when passed a multiple of both 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns "fizzbuzz" when passed number is not divisible by either 3 or 5' do
+    expect(fizzbuzz(2)).to eq 2
   end
 end
